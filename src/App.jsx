7f5754fa109c5b5691c11a7a5bc3f35139615e65
Lakshya Bhardwaj <lakshya.bhardwaj@incedoinc.com>
2021-02-24 19:42:06 +0530

    first edit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import React from 'react';
 // import List from './List';
 // import Greeting from './Greeting';
 import Calculator from './Calculator';
+import Card from './Netflix_App/Card';
+import Sdata from './Netflix_App/Sdata';
+import './index.css';
 
-function App(){
-    return(
-        <>
-            <Calculator />
-        </>
-    );
+const App = () => {
+    return(<>
+        <h1 className='heading_style'> My Favourite OTT Series... </h1>
+        {Sdata.map((val) => {
+            return(
+                <Card 
+                    key={val.id}
+                    imgsrc={val.imgsrc}
+                    title={val.title}
+                    sname={val.sname}
+                    link={val.link}
+                />
+            );
+        })}
+    </>);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,22 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Card from './Netflix_App/Card';
-import Sdata from './Netflix_App/Sdata'
 
 
-ReactDOM.render( <>
-
-  <h1 className='heading_style'> My Favourite OTT Series... </h1>
-  {Sdata.map((val) => {
-    return(
-      <Card 
-        imgsrc={val.imgsrc}
-        title={val.title}
-        sname={val.sname}
-        link={val.link}
-      />
-  );
-  })}
-  </>,
+ReactDOM.render( <App />,
   document.getElementById("root"));
